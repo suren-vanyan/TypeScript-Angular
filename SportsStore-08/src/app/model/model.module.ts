b33cb2bee16647d3ab044ef13345835d537a964a
Suren Vanyan <vanyansuren@hotmail.com>
2019-05-28 23:07:23 +0400

    chnage stroe.component

--- SportsStore-08/src/app/model/model.module.ts
+++ SportsStore-08/src/app/model/model.module.ts
@@ -1,15 +1,8 @@
-import { NgModule } from "@angular/core";
-import { ProductRepository } from "./product.repository";
-import { StaticDataSource } from "./static.datasource";
-import { Cart } from "./cart.model";
-import { Order } from "./order.model";
-import { OrderRepository } from "./order.repository";
-import { RestDataSource } from "./rest.datasource";
-import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from '@angular/core';
+import { ProductRepository } from './product.repository';
+import { StaticDataSource } from './static.datasource';
 
 @NgModule({
-  imports: [HttpClientModule],
-  providers: [ProductRepository, Cart, Order, OrderRepository,
-    { provide: StaticDataSource, useClass: RestDataSource }]
+  providers: [ProductRepository, StaticDataSource]
 })
 export class ModelModule { }
