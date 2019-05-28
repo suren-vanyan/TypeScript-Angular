c04fd7accd21872660e832c669c7fadd655adfc5
Suren Vanyan <vanyansuren@hotmail.com>
2019-05-28 22:56:51 +0400

    chnage all

--- SportsStore/src/app/model/model.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ProductRepository } from '../product.repository';
-import { StaticSource } from '../model/product.repository';
-
-@NgModule({
-    providers: [ProductRepository, StaticSource]
-})
-
-export class ModelModule { }
--- SportsStore/src/app/store/store.component.ts
+++
@@ -1,4 +0,0 @@
-import { Component } from "@angular/core";
-import { Product } from "../model/product.model";
-import { ProductRepository } from "../model/product.repository";
-
